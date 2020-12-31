4176da621020662c5450f4b48ed4da047ef5e49d
MifzalAqmar <mifzal.aqmar@gmail.com>
2020-12-31 09:39:55 +0800

    Initial commit

---
+++ src/MyRunnable.java
@@ -0,0 +1,15 @@
+public class MyRunnable implements Runnable {
+
+    class A extends Runnable {public void run(){}}
+
+    class B extends Object {public void run() {}}
+
+    class C implements Runnable {public void run(){}}
+
+    class D implements Runnable {void run(){}}
+
+    class E implements Runnable {public void start(){}}
+
+
+    }
+
